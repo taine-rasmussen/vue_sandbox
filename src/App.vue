81741b9d053b822ccb7c88a4d4572f92defa2f5f
Taine Rasmussen <taine.rasmussen@gmail.com>
2025-09-28 15:13:00 +0200

    dynamic sizing

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,49 @@
 <script setup lang="ts">
-  import { ref } from 'vue'
+import { ref } from 'vue'
 
-  const gameGrid = ref(Array(9).fill(0))
-  console.log(gameGrid)
-
+const gameGrid = ref(Array(9).fill(0))
 </script>
 
 <template>
   <div class="wrapper">
     <p>Sandbox</p>
     <div class="gameGrid">
-      <div v-for="(cell, index) in gameGrid" class="gameCell">
-        <div :key="index">{{ cell }}</div>
+      <div v-for="(cell, index) in gameGrid" :key="index" class="gameCell">
+        {{ cell }}
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-  p {
-    color: red;
-    font-weight: bold;
-  }
+p {
+  color: red;
+  font-weight: bold;
+}
 
-  .wrapper {
-    display: flex;
-    width: 100%;
-    height: 100%;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
+.wrapper {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 
-  .gameGrid {
-    display: flex;
-    flex-wrap: wrap;
-    width: 456px;
-    padding: 8px;
-  }
+.gameGrid {
+  display: grid;
+  grid-template-columns: repeat(3, minmax(150px, 1fr)); 
+  gap: 8px;
+  padding: 8px;
+}
 
-  .gameCell {
-    display: flex;
-    border: 1px solid blue;
-    height: 150px;
-    width: 150px;
-    align-items: center;
-    justify-content: center;
-  }
+.gameCell {
+  display: flex;
+  border: 1px solid blue;
+  aspect-ratio: 1 / 1; 
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  border-radius: 10%;
+}
 </style>
